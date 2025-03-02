17dea0c604d7064beaa1bbbfeafe3b29f3d83276
Antonios Gkamitsios <angam97@gmail.com>
2025-03-02 23:23:30 +0200

    Add insert method for todos

--- internal/data/todos.go
+++ internal/data/todos.go
@@ -3,10 +3,10 @@ package data
 import "time"
 
 type Todo struct {
-	ID          int64
-	CreatedAt   time.Time
-	Title       string
-	Description string
+	ID        int64
+	CreatedAt time.Time
+	Title     string
+	Done      bool
 }
 
 type TodoModel struct {
@@ -15,4 +15,15 @@ type TodoModel struct {
 
 func (m TodoModel) GetAll() ([]Todo, error) {
 	return m.DB.Todos, nil
+}
+
+func (m TodoModel) Insert(todo *Todo) error {
+	id := len(m.DB.Todos) + 1
+
+	todo.ID = int64(id)
+	todo.CreatedAt = time.Now()
+	todo.Done = false
+	m.DB.Todos = append(m.DB.Todos, *todo)
+
+	return nil
 }
